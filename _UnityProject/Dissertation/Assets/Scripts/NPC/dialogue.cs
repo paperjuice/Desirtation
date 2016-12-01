6d0783c2ca4baa751922ae44bd647f79ada00ad4
paperjuice <paperjuiceinc@gmail.com>
2016-12-01 08:47:39 +0000

    added girl1, started programming text dialogue

---
+++ _UnityProject/Dissertation/Assets/Scripts/NPC/dialogue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue : MonoBehaviour {
+
+    [SerializeField]private GameObject[] dialoguesGO;
+    [SerializeField] private GameObject speakToText;      //the text shown when you are close enough to an NPC and you can talk
+
+    private GameObject player;
+    private GameObject dialogueParent;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        dialogueParent = GameObject.FindGameObjectWithTag("dialogueFather");
+    }
+
+    void Update()
+    {
+        ActivateDialogue();
+    }
+
+    void ActivateDialogue()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) < 3)
+        {
+            speakToText.gameObject.SetActive(true);
+
+            
+
+        }
+        else 
+        {
+            speakToText.gameObject.SetActive(false);
+        }
+    }
+}

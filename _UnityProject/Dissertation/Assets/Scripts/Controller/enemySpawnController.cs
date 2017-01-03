27222cf029fe388aa52dcaecc2e8b956a7f1f089
paperjuice <paperjuiceinc@gmail.com>
2017-01-03 07:53:17 +0000

    started working on spawnEnemiesController

---
+++ _UnityProject/Dissertation/Assets/Scripts/Controller/enemySpawnController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawnController : MonoBehaviour {
+
+
+    [SerializeField] GameObject[] enemyList;
+    [SerializeField] GameObject[] bossList;
+    private int i=1;
+
+
+
+
+    void Start()
+    {
+        if (controller.dungeonLevel % 5 == 0)
+            i++;
+    }
+
+    void SpawnFoes()
+    {
+
+    }
+
+
+
+
+}

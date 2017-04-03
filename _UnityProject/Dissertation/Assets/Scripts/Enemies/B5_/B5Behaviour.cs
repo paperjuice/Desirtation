319d8c76eefd1e7325ce5e75d82193ea855662ce
paperjuice <paperjuiceinc@gmail.com>
2017-04-03 07:53:42 +0100

    boss5 added attacks

--- _UnityProject/Dissertation/Assets/Scripts/Enemies/B5_/B5Behaviour.cs
+++ _UnityProject/Dissertation/Assets/Scripts/Enemies/B5_/B5Behaviour.cs
@@ -7,6 +7,7 @@ public class B5Behaviour : MonoBehaviour {
 	
 	GameObject player;
 	[SerializeField] float rs;
+	Rigidbody _rigid;
 
 
 	//movement
@@ -15,8 +16,16 @@ public class B5Behaviour : MonoBehaviour {
 	float end_time = 2;
 	int random_movement = 0;
 
+	//dash
+	[HeaderAttribute("Dash Power")] 
+	[SerializeField] float power;
+	int rollDash;
+	Vector3 dashRandomPosition;
+
 	//Attack
 	[SerializeField] Animator anim;
+	[SerializeField] List<string> listOfAttacks;
+	int chooseAttack;
 	int chanceToAttack = 0;
 	float current_chooseAttack;
 	float end_chooseAttack = 3;
@@ -26,12 +35,13 @@ public class B5Behaviour : MonoBehaviour {
 	void Awake()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
+		_rigid = GetComponent<Rigidbody>();
 	}
 
 	void Update()
 	{
 		RotateTowardsPlayer();
-		if(!isAttacking)
+		// if(!isAttacking)
 			Movement();
 
 		SetAttackingToFalse();
@@ -46,6 +56,7 @@ public class B5Behaviour : MonoBehaviour {
 			{
 				random_movement = Random.Range(0,10);
 				time_between_random_movement = 0f;
+			//	RollDash();
 			}
 		}
 		else if(Vector3.Distance(transform.position, player.transform.position) >= 10)
@@ -65,9 +76,10 @@ public class B5Behaviour : MonoBehaviour {
 		{
 				transform.position -= transform.right * Time.deltaTime * ms * 2;
 		}
-		if(random_movement >=3)	
+		if(random_movement >=3 && !isAttacking)	
 		{	
-				//aici attaca
+				Attack();
+				isAttacking=true;
 		}
 	}
 
@@ -83,16 +95,38 @@ public class B5Behaviour : MonoBehaviour {
 
 	void Attack()
 	{
+		chooseAttack  = Random.Range(0, listOfAttacks.Count);
+		if(chooseAttack==0 || chooseAttack==3 )
+			StartCoroutine(WaitForCombo1());
 
+		anim.SetTrigger(listOfAttacks[chooseAttack]);
+	}
+
+	IEnumerator WaitForCombo1()
+	{
+		yield return new WaitForSeconds(1);
+		anim.SetTrigger("combo1.1");
+		transform.position = player.transform.position - transform.forward* 3f;
 	}
 
 	void SetAttackingToFalse()
 	{
 		if(anim.GetCurrentAnimatorStateInfo(0).IsName("Armature|idle") && isAttacking)
 		{
-			isAttacking = true;
+			isAttacking = false;
 		}
 	}
+
+	void RollDash()
+	{
+		print("dash");
+		rollDash = Random.Range(0,10);
+		dashRandomPosition = new Vector3(transform.position.x + Random.Range(-3,3), transform.position.y , transform.position.z + Random.Range(-3,3));
+		if(rollDash <6)
+			_rigid.AddForce(((transform.position - dashRandomPosition).normalized) * Time.fixedDeltaTime * power * 60000); 
+
+	}
+
 
 
 }

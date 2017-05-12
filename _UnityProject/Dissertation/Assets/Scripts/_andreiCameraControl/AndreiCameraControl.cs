cd90aa53afd51da80e13570123a927e44384b6c0
paperjuice <paperjuiceinc@gmail.com>
2017-05-12 22:22:37 +0100

    minor changes

---
+++ _UnityProject/Dissertation/Assets/Scripts/_andreiCameraControl/AndreiCameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndreiCameraControl : MonoBehaviour {
+
+	
+
+	bool isBehaviourActive = false;
+
+
+
+	void Update()
+	{
+		CameraBehaviour();
+	}
+
+	void CameraBehaviour()
+	{
+		float mouse_x = Input.GetAxis("Mouse X");
+		float mouse_y = Input.GetAxis("Mouse Y") ;
+
+		Quaternion target = Quaternion.Euler(-mouse_x,mouse_y,0) * transform.rotation;
+
+		transform.rotation = Quaternion.Lerp(transform.rotation, target, Time.deltaTime * 2);
+
+
+	}
+
+
+
+}
